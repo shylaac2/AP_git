90ff96204f234db334e5e40dd90c9d08d387bdc1
Shyla Choy <20543365@student.westernsydney.edu.au>
2024-05-17 18:02:54 +1000

    Committing in new branch

--- 1.R
+++ 1.R
@@ -2,4 +2,5 @@ setwd('~/Documents/tmpgit/')
 data = read.csv('1.csv')
 data$t=as.Date(data$t, origin="2020-03-01")
 data_sub=subset(data, subset = t>="2020-07-01" & t<= "2020-12-31")
-plot(data$H,data$t, type='l')
+plot(data$t,data$H, type='l', col='RED', main='COVID Plot', 
+     ylab='Hospital Beds', xlab='Dates')
