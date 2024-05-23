4877627d8181c84e5ec24f6da49722c0ebdb9a62
Shyla Choy <20543365@student.westernsydney.edu.au>
2024-05-23 16:18:59 +1000

    Testing difference 2

--- 1.R
+++ 1.R
@@ -4,4 +4,3 @@ data$t=as.Date(data$t, origin="2020-03-01")
 data_sub=subset(data, subset = t>="2020-07-01" & t<= "2020-12-31")
 plot(data$t,data$H, type='l', col='RED', main='COVID Plot', 
      ylab='Hospital Beds', xlab='Dates')
-## Hi
